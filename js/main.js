ff5447771f46c642f1803eb96f05b6c8bbcb6fd3
Artemis <69371778+fokstreet@users.noreply.github.com>
2026-08-13 20:35:06 +0300

    add: 2slider

--- js/main.js
+++ js/main.js
@@ -43,9 +43,33 @@ mMenuToggle.addEventListener("click", (event) => {
     menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-const swiper = new Swiper('.swiper', {
+const swiperSteps = new Swiper(".steps-slider", {
     speed: 400,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".steps-button-next",
+        prevEl: ".steps-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+
+        768: {
+            slidesPerView: 3,
+
+        },
+
+        1024: {
+            slidesPerView: 4,
+
+        },
+    },
+});
+
+const swiper = new Swiper('.features-slider', {
+    speed: 400,
+    slidesPerView: 1,
     navigation: {
         nextEl: ".slider-button-next",
         prevEl: ".slider-button-prev",
